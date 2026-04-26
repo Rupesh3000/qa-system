71a61a6e6fe76cb8033cc1011a92281cfce6bf95
Rupesh3000 <rupeshpatil11121@gmail.com>
2026-04-26 23:09:33 +0530

    feat: add RAG setup script with Google embeddings and ChromaDB vector store

---
+++ backend/ setup_rag.py
@@ -0,0 +1,45 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+my_key = os.getenv("GEMINI_EMBEDDING_API_KEY")
+
+
+## load document
+
+file_path = "/home/Mr.nobody_3000/Desktop/Code/AI/Mark_1/backend/Company_Docs/skills-and-behaviours-for-customer-care-entry-3.pdf"
+loader = PyPDFLoader(file_path)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+)
+
+# Splitting just the first page to save memory
+texts = text_splitter.split_documents([docs[0]])
+
+
+
+# --- 3. Embedding ---
+
+
+embeddings_model = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-001", google_api_key=my_key
+)
+
+vector_sotre = Chroma.from_documents(
+    documents=texts,
+    embedding=embeddings_model,
+    persist_directory="./Vector"
+)
+
+
+
+
+
